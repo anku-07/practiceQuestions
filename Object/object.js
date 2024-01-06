807b40b082203454b73beea5c95ad35cd281affe
anku-07 <adityamondal48765@gmail.com>
2024-01-07 01:30:54 +0530

    Added object folder

---
+++ Object/object.js
@@ -0,0 +1,18 @@
+/*
+Create an object representing a person with properties like name, age, and gender.
+Access and print the age of the person.
+Add a new property, 'job', to the person object.
+*/
+// Qn -1
+
+// Define a object name of person.
+const person = {
+    name : "Aditya",
+    age : 22,
+    gender : "male"
+};
+console.log(person.age); // print the age of person.
+
+// Add a key value pair job
+person.job = 'Software Engineer';
+console.log(person);
