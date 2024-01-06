1731b68d486425c79e0ea04912bc6ec8422e16c1
anku-07 <adityamondal48765@gmail.com>
2024-01-07 01:38:10 +0530

    Added loop folder

---
+++ Loop/loop.js
@@ -0,0 +1,23 @@
+/*
+Write a for loop to print numbers from 1 to 5.
+Use a while loop to display numbers from 5 to 1.
+Iterate through an array using a for loop and print each element.
+*/
+//Qn -1
+
+for(let i = 1; i <= 5; i++){
+    console.log(i);
+}
+
+// Qn-2
+
+for(let i = 5; i >= 1; i--){
+    console.log(i);
+}
+
+// Qn -3
+
+let arr = ['adi','arghya','hriday','akash','sayan'];
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
