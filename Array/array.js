ce0a796e9561ff389f8a42987ac434dacfca5db0
anku-07 <adityamondal48765@gmail.com>
2024-01-07 01:22:56 +0530

    Array file added

---
+++ Array/array.js
@@ -0,0 +1,16 @@
+/*
+Create an array with fruits (e.g., apple, banana, orange).
+Access the second element of the array.
+Add an element to the end of the array and remove the first element.
+*/
+//Qn-1
+
+let fruits = ['apple','banana','orange'];//Define a array
+
+console.log(fruits[1]);
+
+//Qn-2
+
+console.log(fruits.push('mango'));
+
+console.log(fruits.shift());
