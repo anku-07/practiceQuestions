5457d9b3fc4be333f56a698c35b9ef2113de45e3
anku-07 <adityamondal48765@gmail.com>
2024-01-07 01:59:44 +0530

    Added string folder

---
+++ String/string.js
@@ -0,0 +1,17 @@
+/*
+Concatenate two strings together.
+Find the length of a string.
+Convert a string to uppercase or lowercase.
+*/
+
+//Qn - 1
+let str1 = "Aditya";
+let str2 = "Priyanka";
+let concatStr = str1 + str2
+console.log(concatStr);
+
+//Qn-2
+console.log(concatStr);
+//Qn-3
+console.log(concatStr.toUpperCase());
+console.log(concatStr.toLocaleLowerCase());
