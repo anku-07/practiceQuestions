11162b40b46bb4d94c413c4f0be8af04cfa5dc70
anku-07 <adityamondal48765@gmail.com>
2024-01-13 01:22:16 +0530

    Implement a function to find the maximum of two numbers

---
+++ Function/maximumOfTwoNumber.js
@@ -0,0 +1,9 @@
+// Implement a function to find the maximum of two numbers.
+function maxOfTwoNum(a,b){
+    if(a > b){
+        console.log(`${a} is greatest`);
+    }else{
+        console.log(`${b} is greatest`)
+    }
+}
+maxOfTwoNum(1,2);
