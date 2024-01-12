6871432bb2f056c90f32f2fca381f0b074424a26
anku-07 <adityamondal48765@gmail.com>
2024-01-13 01:28:35 +0530

    Print hello world after 3 sec

---
+++ Function/SetTimeOut/printHWafter3Sec.js
@@ -0,0 +1,6 @@
+// Create a timer that logs "Hello, World!" after 3 seconds.
+console.log("Wait 3 sec");
+setTimeout(() => {
+    console.log("Hello World !");
+    console.log("Thanks for watching");
+},3000);
