3a1ca2b82728a04f25baf80f778497a41934251d
anku-07 <adityamondal48765@gmail.com>
2024-01-25 23:51:07 +0530

    upeerCase lowerCase file added

---
+++ String/upperCaseLowerCase.js
@@ -0,0 +1,6 @@
+// Convert the string "Hello World" to uppercase and then to lowercase without using functions.
+let str = "Hello World!";
+let NewStr = str.toUpperCase();
+console.log(NewStr);
+let newStr = str.toLowerCase();
+console.log(newStr);
