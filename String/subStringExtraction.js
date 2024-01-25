39c0986eaf5f7ccbbd80a1a4dec42b8ebebb2b9d
anku-07 <adityamondal48765@gmail.com>
2024-01-25 23:59:24 +0530

    substring extraction

---
+++ String/subStringExtraction.js
@@ -0,0 +1,4 @@
+// Extract a substring from the string "JavaScript" starting at index 2 and ending at index 6.
+let str = "JavaScript";
+let subStr = str.substring(2,6) // vaSc
+console.log(subStr);
