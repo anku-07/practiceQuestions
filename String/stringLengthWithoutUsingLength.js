1dc8b54a990bd7118b69b2700f52499a38cb4a83
anku-07 <adityamondal48765@gmail.com>
2024-01-26 00:07:43 +0530

    string length without using length

---
+++ String/stringLengthWithoutUsingLength.js
@@ -0,0 +1,7 @@
+//Find and display the length of the string "Programming" without using the length property.
+let str = "Programming";
+let length = 0;
+for(let char of str){
+    length++;
+}
+console.log("Length of string : ",length);
