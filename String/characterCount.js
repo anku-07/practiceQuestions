5782e4e0554df2c32db61acc629c5256bd5e930b
anku-07 <adityamondal48765@gmail.com>
2024-01-26 23:44:22 +0530

    Modified

--- String/characterCount.js
+++ String/characterCount.js
@@ -1,11 +1,15 @@
 //Write a function that takes a string and a character as parameters and counts the number of occurrences of that character in the string.
 
-let str = "Banana";
-let char = 'a';
-let count = 0;
-for(let fruit of str){
-    if(str[fruit]==char){
-        count++;
+function numOfoccurrences(str,char){
+    let count = 0;
+    for(let i = 0; i <= str.length-1;i++){
+        if(str[i] == char){
+            count++;
+        }
     }
+    return count;
 }
-console.log(count);
+let name = "aditya";
+let char = 'a';
+console.log(numOfoccurrences(name,char));
+console.log(numOfoccurrences('aditya','a')); // we can do that;
