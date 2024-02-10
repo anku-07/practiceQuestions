6daf122b24b25cb6944d94ceaf3cd7bab20b07dc
anku-07 <adityamondal48765@gmail.com>
2024-02-10 13:24:59 +0530

    Add new file

---
+++ Object/Q-1.js
@@ -0,0 +1,8 @@
+let student = {
+    name : "Aditya",
+    roll : 1,
+    city : "Kolkata"
+}
+console.log(student);
+let studentCity = student.city;
+console.log(studentCity)
---
+++ Object/Q-2.js
@@ -0,0 +1,10 @@
+let info = {
+    name : "Aditya",
+    passion : "Software Enginner",
+    company : "Amazon",
+    expirience : 2
+};
+
+console.log(info);
+info.city = "Kolkata"; // adding new prop
+
